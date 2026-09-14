631cfe3d80ff88cb273299c92d528634895798b2
Junseok-22 <kjscan1@gmail.com>
2026-09-14 14:39:39 -0400

    add barycentric and triangle fucntions

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <algorithm>
 
 constexpr TGAColor red = {0,0,255,255};
 
 struct Vec3f {float x, y, z;};
+struct Vec2i {int x, y;};
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color) {
     bool steep = false;
@@ -35,6 +37,39 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor &color
             image.set(x, y, color);
     }
 }
+
+Vec3f barycentric(Vec2i a, Vec2i b, Vec2i c, Vec2i p) {
+    Vec3f vx = {float(c.x - a.x), float(b.x - a.x), float(a.x - p.x)};
+    Vec3f vy = {float(c.y - a.y), float(b.y - a.y), float(a.y - p.y)};
+    Vec3f vu = {vx.y * vy.z - vx.z * vy.y, vx.z * vy.x - vx.x * vy.z, vx.x * vy.y - vx.y * vy.x};
+
+    if (std::abs(vu.z) < 1) 
+        return Vec3f{-1, 1, 1}; // triangle is degenerate, in this case return something with negative coordinates
+
+    else 
+        return Vec3f{1.0f - (vu.x + vu.y) / vu.z, vu.y / vu.z, vu.x / vu.z};
+}
+
+void triangle(Vec2i a, Vec2i b, Vec2i c, TGAImage &image, const TGAColor &color) {
+    int minx = std::min({a.x, b.x, c.x});
+    int maxx = std::max({a.x, b.x, c.x});
+    int miny = std::min({a.y, b.y, c.y});
+    int maxy = std::max({a.y, b.y, c.y});
+
+    minx = std::max(0, minx);                    // don't start left of the image
+    maxx = std::min(image.width() - 1, maxx);    // don't go past the right edge
+    miny = std::max(0, miny);                    // don't start above the image
+    maxy = std::min(image.height() - 1, maxy);   // don't go past the bottom
+
+    for (int x = minx; x <= maxx; x++){
+        for (int y = miny; y <= maxy; y++){
+            Vec3f bc = barycentric(a, b, c, {x,y});
+            if (bc.x < 0 || bc.y < 0 || bc.z < 0) continue; 
+            image.set(x, y, color);
+        }
+    }
+}   
+
 
 int main() {
     constexpr int width = 800;
@@ -70,17 +105,17 @@ int main() {
     
     for (int i = 0; i < faces.size(); i++) {
         std::vector<int> face = faces[i];
-        for (int j = 0; j < face.size(); j++) {
-            Vec3f v0 = verts[face[j]];
-            Vec3f v1 = verts[face[(j + 1) % 3]];
+        
+        Vec3f w0= verts[face[0]];
+        Vec3f w1 = verts[face[1]];
+        Vec3f w2 = verts[face[2]];
 
-            int x0 = (v0.x + 1.0) * width  / 2.0;
-            int y0 = (v0.y + 1.0) * height / 2.0;
-            int x1 = (v1.x + 1.0) * width  / 2.0;
-            int y1 = (v1.y + 1.0) * height / 2.0;
+        Vec2i p0 = { int((w0.x + 1.0) * width  / 2.0), int((w0.y + 1.0) * height / 2.0) };
+        Vec2i p1 = { int((w1.x + 1.0) * width  / 2.0), int((w1.y + 1.0) * height / 2.0) };
+        Vec2i p2 = { int((w2.x + 1.0) * width  / 2.0), int((w2.y + 1.0) * height / 2.0) };
 
-            line(x0, y0, x1, y1, framebuffer, red);
-        }
+        triangle(p0, p1, p2, framebuffer, red);
+
     }
 
     framebuffer.write_tga_file("output.tga");
